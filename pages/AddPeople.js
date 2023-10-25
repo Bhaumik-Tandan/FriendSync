0cc8b04c87796c367cd2e20d8519397d63389015
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2023-10-25 20:18:08 +0530

    [ADD] maximum date as current date

--- pages/AddPeople.js
+++ pages/AddPeople.js
@@ -1,37 +1,47 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 function AddPeople({ navigation }) {
   const [name, setName] = useState('');
-  const [birthday, setBirthday] = useState('');
+  const [birthday, setBirthday] = useState(new Date());
   const [description, setDescription] = useState('');
 
   const handleAddPerson = () => {
-    // Here, you can save the person's information to your local storage or database.
-    // You can use AsyncStorage, SQLite, or any other storage method of your choice.
-    // For simplicity, we'll just log the entered data for now.
     console.log('Name:', name);
     console.log('Birthday:', birthday);
     console.log('Description:', description);
+    navigation.goBack();
+  };
 
-    // You can navigate back to the list of people or any other relevant screen after adding the person.
-    navigation.goBack(); // Navigates back to the previous screen.
+  const onDateChange = (event, selectedDate) => {
+    if (selectedDate) {
+      setBirthday(selectedDate);
+    }
   };
 
   return (
-    <View>
-      <Text>Add New Person</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Add New Person</Text>
       <TextInput
+        style={styles.input}
         placeholder="Name"
         value={name}
         onChangeText={text => setName(text)}
       />
+      <View style={styles.birthdayContainer}>
+        <Text style={styles.label}>Birthday</Text>
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={birthday}
+          mode="date"
+          display="default"
+          onChange={onDateChange}
+          maximumDate={new Date()}
+        />
+      </View>
       <TextInput
-        placeholder="Birthday"
-        value={birthday}
-        onChangeText={text => setBirthday(text)}
-      />
-      <TextInput
+        style={styles.input}
         placeholder="Description"
         value={description}
         onChangeText={text => setDescription(text)}
@@ -41,5 +51,32 @@ function AddPeople({ navigation }) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 8,
+  },
+  birthdayContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  label: {
+    marginRight: 10,
+    fontSize: 16,
+  },
+});
 
 export default AddPeople;
